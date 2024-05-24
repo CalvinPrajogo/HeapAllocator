4597ca28062fdaf01b04b6d0035e549451d9964a
Calvin Prajogo <calprajogo@gmail.com>
2024-05-23 22:08:58 -0500

    Create heap.c and heap.h declaring heap_init, heap_alloc, and heap_free

---
+++ heap.c
@@ -0,0 +1,17 @@
+#include "heap.h"
+
+//TODO: Implement heap_init
+// Initialize giant chunk of free space as the heap
+void heap_init(int size) {
+    
+}
+
+//TODO: Implement heap_alloc
+void* heap_alloc() {
+
+}
+
+//TODO: Implement heap_free
+void heap_free() {
+
+}
---
+++ heap.h
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <string.h>
+
+typedef struct {
+    int size;
+} header;
+
